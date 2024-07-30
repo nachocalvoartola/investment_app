8ba39c7591ab889487be71974c80276b9a5cd593
nachocalvoartola <nachocalvoartola@gmail.com>
2024-07-30 18:03:30 +0200

    create main component

--- front_investment_app/src/app/app.component.ts
+++ front_investment_app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InvestmentAppComponent } from './components/investment-app.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, InvestmentAppComponent],
+  templateUrl: './app.component.html'  
 })
 export class AppComponent {
   title = 'front_investment_app';
---
+++ front_investment_app/src/app/components/investment-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'investment-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-app.component.html'  
+})
+export class InvestmentAppComponent {
+
+  title: string = 'Investment app'
+
+}
