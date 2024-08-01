194676edf703bedb6f697c03cc7f4d7833d05ed1
nachocalvoartola <nachocalvoartola@gmail.com>
2024-08-01 12:18:15 +0200

    Add carousel component

--- front_investment_app/src/app/components/investment-app.component.ts
+++ front_investment_app/src/app/components/investment-app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'investment-app',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, CarouselComponent],
   templateUrl: './investment-app.component.html'  
 })
 export class InvestmentAppComponent {
